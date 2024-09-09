16ae372ab996a13dd994c638fa3907be6ab1636e
Kei Sugano <tobasojyo@gmail.com>
2024-09-10 07:15:52 +0900

    feature/nanaism-san-s-suggestion-on-twitter-card  https://github.com/nanaism/hoge20240909/blob/main/src/components/Header.js 適用

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { Helmet } from 'react-helmet';
-import '../styles.css';
-import Ham from './Ham';
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import "../styles.css";
+import Ham from "./Ham";
 
 function Header({ bible, title, description, url, image }) {
-  const [pageType, setState] = useState('norm');
+  const [pageType, setState] = useState("norm");
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const currentRuby = urlParams.get('type');
-    setState(currentRuby || 'norm');
+    const currentRuby = urlParams.get("type");
+    setState(currentRuby || "norm");
   }, []);
 
   const toggleRuby = () => {
     const urlParams = new URLSearchParams(window.location.search);
     let newState;
 
-    if (pageType === 'norm') {
-      newState = 'ruby';
-    } else if (pageType === 'ruby') {
-      newState = 'en';
+    if (pageType === "norm") {
+      newState = "ruby";
+    } else if (pageType === "ruby") {
+      newState = "en";
     } else {
-      newState = 'norm';
+      newState = "norm";
     }
 
-    urlParams.set('type', newState);
+    urlParams.set("type", newState);
     window.location.search = urlParams.toString(); // Reload the page with the new state
 
     setState(newState); // Update the state
@@ -34,13 +34,13 @@ function Header({ bible, title, description, url, image }) {
     const urlParams = new URLSearchParams(window.location.search);
     let newState;
 
-    if (pageType === 'norm') {
-      newState = 'en';
+    if (pageType === "norm") {
+      newState = "en";
     } else {
-      newState = 'norm';
+      newState = "norm";
     }
 
-    urlParams.set('type', newState);
+    urlParams.set("type", newState);
     window.location.search = urlParams.toString(); // Reload the page with the new state
 
     setState(newState); // Update the state
@@ -50,14 +50,14 @@ function Header({ bible, title, description, url, image }) {
     <div className="header">
       <Helmet>
         <meta charSet="UTF-8" />
-        <link rel="icon" href="/static/favicon/favicon.ico" />
+        <link rel="icon" href={`${process.env.PUBLIC_URL}/static/favicon/favicon.ico`} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title || "ASMRキリスト教会"}</title>
         <meta name="description" content={description || "10代〜20代の若者へ..."} />
         <meta name="keywords" content="聖書, キリスト教, ASMR, 教会" />
-        
+                
         {/* Open Graph / Facebook */}
-        <meta property="og:type" content="article" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content={title || "ASMRキリスト教会"} />
         <meta property="og:description" content={description || "10代〜20代の若者へ..."} />
         <meta property="og:url" content={url || "https://www.asmrchurch.com"} />
@@ -65,9 +65,10 @@ function Header({ bible, title, description, url, image }) {
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@asmrchurch" />
+        <meta name="twitter:creator" content="@asmrchurch" />
         <meta name="twitter:title" content={title || "ASMRキリスト教会"} />
         <meta name="twitter:description" content={description || "10代〜20代の若者へ..."} />
-        <meta name="twitter:url" content={url || "https://www.asmrchurch.com"} />
         <meta name="twitter:image" content={image || "https://www.asmrchurch.com/static/images/kirikan.jpg"} />
       </Helmet>
 
@@ -77,7 +78,7 @@ function Header({ bible, title, description, url, image }) {
           <>
             <span className="sep"></span>
             <button className="toggle-button" onClick={toggleRuby}>
-              {pageType === 'norm' ? '聖' : pageType === 'ruby' ? 'せ' : 'E'}
+              {pageType === "norm" ? "聖" : pageType === "ruby" ? "せ" : "E"}
             </button>
           </>
         )}
@@ -85,39 +86,55 @@ function Header({ bible, title, description, url, image }) {
           <>
             <span className="sep"></span>
             <button className="toggle-button" onClick={toggleLang}>
-              {pageType === 'norm' ? '聖' : 'E'}
+              {pageType === "norm" ? "聖" : "E"}
             </button>
           </>
         )}
       </div>
 
       <div className="c3">
-        <a target="_blank" rel="noopener noreferrer" href="https://x.com/asmrchurch">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://x.com/asmrchurch"
+        >
           <img
             src={`${process.env.PUBLIC_URL}/static/images/x.png`}
             alt="X (formerly Twitter) logo"
-            style={{ width: '35px', height: '35px' }}
+            style={{ width: "35px", height: "35px" }}
           />
         </a>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@asmrchurch">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.youtube.com/@asmrchurch"
+        >
           <img
             src={`${process.env.PUBLIC_URL}/static/images/youtube.png`}
             alt="YouTube logo"
-            style={{ width: '35px', height: '35px' }}
+            style={{ width: "35px", height: "35px" }}
           />
         </a>
-        <a target="_blank" rel="noopener noreferrer" href="https://asmrchruch.bandcamp.com/">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://asmrchruch.bandcamp.com/"
+        >
           <img
             src={`${process.env.PUBLIC_URL}/static/images/bandcamp.png`}
             alt="Bandcamp logo"
-            style={{ width: '35px', height: '35px' }}
+            style={{ width: "35px", height: "35px" }}
           />
         </a>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.tiktok.com/@sexybible">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.tiktok.com/@sexybible"
+        >
           <img
             src={`${process.env.PUBLIC_URL}/static/images/tiktok.png`}
             alt="TikTok logo"
-            style={{ width: '35px', height: '35px' }}
+            style={{ width: "35px", height: "35px" }}
           />
         </a>
       </div>
