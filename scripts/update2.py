844b2bcba6a824af38ff0eafbf53e58627a1bb5e
Kei Sugano <tobasojyo@gmail.com>
2024-09-10 22:11:55 +0900

    こうきゅうかん

--- scripts/update2.py
+++ scripts/update2.py
@@ -10,9 +10,16 @@ filename = f"public/static/html/norm/{section}.htm"
 with open(filename, 'r') as file:
     for line in file:
         line = line.strip()
+
+        if '<div style="display:block;margin-left:32px;text-indent:0px;">' in line:
+            line = line.replace(
+                '<div style="display:block;margin-left:32px;text-indent:0px;">',
+                '<div style="display:block;margin-left:32px;text-indent:0px;" id="verse">'
+            )
+
         if("</em>" in line):
             em, text = line.split("</em>" )
-            text = f"<div id='verse'>{text}</div>"
+            text = f"<span id='verse'>{text}</span>"
             lines.append(f"{em}</em>{text}")
         else:
             lines.append(line)
