32077423f7048fcfb229c43bfeebdaab85f712ac
Kei Sugano <tobasojyo@gmail.com>
2024-09-10 21:30:49 +0900

    xxx

---
+++ scripts/update2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+section=sys.argv[1]
+
+lines = []
+
+filename = f"public/static/html/norm/{section}.htm"
+with open(filename, 'r') as file:
+    for line in file:
+        line = line.strip()
+        if("</em>" in line):
+            em, text = line.split("</em>" )
+            text = f"<span id='verse'>{text}</span>"
+            lines.append(f"{em}</em>{text}")
+
+linestr = "".join(lines)
+with open(filename, 'w') as file:
+    file.write(linestr)
+
