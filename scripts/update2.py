60895446eb5088d139a2249e896430c941a7cf13
Kei Sugano <tobasojyo@gmail.com>
2024-09-10 21:54:19 +0900

    xxx

--- scripts/update2.py
+++ scripts/update2.py
@@ -12,7 +12,7 @@ with open(filename, 'r') as file:
         line = line.strip()
         if("</em>" in line):
             em, text = line.split("</em>" )
-            text = f"<span id='verse'>{text}</span>"
+            text = f"<div id='verse'>{text}</div>"
             lines.append(f"{em}</em>{text}")
         else:
             lines.append(line)
