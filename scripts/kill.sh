2a68fe679640a5069bff334f9f2acd2ea466a38b
Kei Sugano <tobasojyo@gmail.com>
2024-09-16 12:31:18 +0900

    kill update

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,1 +1,6 @@
-sudo kill $(sudo lsof -t -i:80)
+if sudo lsof -t -i:80 > /dev/null; then
+    sudo kill $(sudo lsof -t -i:80)
+    echo "Killed process on port 80."
+else
+    echo "No process running on port 80."
+fi
